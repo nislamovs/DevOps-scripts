70175bbb813b1b7cc615b2f0d0f98c4a9931f2e1
nislamovs <nizamiislamovs@gmail.com>
2020-11-05 03:45:39 +0200

    Springboot elk stack example added. Cleanup.

--- Docker/docker-elk/springproject/src/main/resources/logback.groovy
+++ Docker/docker-elk/springproject/src/main/resources/logback.groovy
@@ -1,20 +1,8 @@
 import net.logstash.logback.appender.LogstashTcpSocketAppender
 import net.logstash.logback.encoder.LogstashEncoder
-//import org.springframework.core.io.ClassPathResource
-//import org.yaml.snakeyaml.Yaml
 
 String appName = "elkexample"
 String profile = "dafault"
-
-//def bootstrap = new ClassPathResource("bootstrap.yml")
-//def application = new ClassPathResource("application.yml")
-//def resource = bootstrap.exists() ? bootstrap :
-//        application.exists() ? application : null
-//if (resource) {
-//    def yaml = new Yaml()
-//    appName = yaml.load(resource.inputStream).spring.application.name
-//}
-
 def LOGSTASH_SERVER = "logstash:5000"
 
 println "=" * 80
@@ -25,7 +13,6 @@ println """    APP NAME          : $appName
 println "=" * 80
 
 def appenders = []
-//if (LOGSTASH_SERVER) {
 appender("TCP", LogstashTcpSocketAppender.class) {
     destination = LOGSTASH_SERVER
     encoder(LogstashEncoder) {
@@ -33,7 +20,6 @@ appender("TCP", LogstashTcpSocketAppender.class) {
     }
 }
 appenders += "TCP"
-//}
 
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
